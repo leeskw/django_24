96104ab67a297ea0f766b341cbf20a9788b3e80b
leeskw <leeskw@nate.com>
2024-02-02 21:20:03 -0800

    added static root

--- django_24/settings.py
+++ django_24/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR/'static']
 
+STATIC_ROOT = BASE_DIR / 'assets'
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'img/'
